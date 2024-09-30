e6858e6bfd8093e54ba5902433a8b399b33cded4
adityashitole <160098275+adityashitole@users.noreply.github.com>
2024-09-30 15:10:48 +0530

    Add files via upload

---
+++ slip26.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class slip26
+{
+public static void main(String args[])
+{
+char ch1='a';
+char ch2='b';
+char ch3='B';
+int asciivalue1=ch1;
+int asciivalue2=ch2;
+int asciivalue3=ch3;
+System.out.println("the asciivalue of  "+ch1+"value:"+asciivalue1);
+System.out.println("the asciivalue of  "+ch2+"value:"+asciivalue2);
+System.out.println("the asciivalue of  "+ch3+"value:"+asciivalue3);
+}
+}
